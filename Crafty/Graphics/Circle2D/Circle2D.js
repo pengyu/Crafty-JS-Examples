eaf934be1bb8a2d496eb8aa0647b4f97954ec3ab
Peng Yu <pengyu.ut@gmail.com>
2012-03-09 20:24:55 -0600

    Circle2D related

--- Crafty/Graphics/Circle2D/Circle2D.js
+++ Crafty/Graphics/Circle2D/Circle2D.js
@@ -1,20 +1,68 @@
 Crafty.c("Circle2D", {
     _radius: 8,
+    _friction_constant: .997,
     fillStyle:"rgba(0,255,0,1)",
     strokeStyle: "rgba(0,0,0,0)",
     ready: true,
 
-    get radius() { return this._radius; },
-    set radius(r) {
-      this._radius = r;
-      this.w = this.h = 2*this.radius;
+    getCenterX: function() { return this.x+this._radius;},
+    getCenterY: function() { return this.y+this._radius;},
+
+    /*
+     getRadius: function() { return this._radius; },
+     setRadius: function(r) {
+     this._radius = r;
+     this.w = this.h = 2*this.radius;
+     this.trigger("Change");
+   },
+
+   setCircleX(x): function {
+   this.x=x-this._radius;
+   this.trigger("Change");
+ },
+
+ setCircleY(y) {
+ this.y=y-this._radius;
+ this.trigger("Change");
+    },
+    */
+
+    get vel() {
+      return {
+        vx: this.vx
+        , vy: this.vy
+      };
+    },
+    set vel(v) {
+      this.vx=v['vx'];
+      this.vy=v['vy'];
       this.trigger("Change");
     },
 
-    init: function() {
-      //this.requires("2D, Canvas");
+    addVelocity: function(vx, vy) {
+      this.vx+=vx;
+      this.vy+=vy;
 
-      this.w = this.h = 2*this.radius;
+      this.trigger("Change");
+    },
+
+    step: function() {
+      this.x += this.vx;
+      this.y += this.vy;
+      if(this.x>400) {
+        this.x-=400;
+      }
+      if(this.y>400) {
+        this.y-=400;
+      }
+      this.vx *= this._friction_constant;
+      this.vy *= this._friction_constant;
+    },
+
+    init: function() {
+      this.requires("2D, Canvas");
+
+      this.w = this.h = 2*this._radius;
       var TWO_PI = Math.PI*2;
 
       var draw=function(e) {
@@ -22,7 +70,7 @@ Crafty.c("Circle2D", {
           e.ctx.fillStyle = this.fillStyle;
           e.ctx.strokeStyle = this.strokeStyle;
           e.ctx.beginPath();
-          e.ctx.arc(this.x, this.y, this.radius, 0, TWO_PI, true);
+          e.ctx.arc(this.x + this._radius, this.y + this._radius, this._radius, 0, TWO_PI, true);
           e.ctx.closePath();
           e.ctx.fill();
         }
@@ -35,8 +83,24 @@ Crafty.c("Circle2D", {
       //this.trigger("Change");
     },
 
-    circle: function(radius) {
-      this.radius = radius;
+    circleCenter: function(x, y) {
+      this.x = x - this._radius;
+      this.y = y - this._radius;
+      this.trigger("Change");
+      return this;
+    },
+
+    circleRadius: function(radius) {
+      this._radius = radius;
+      this.x = this.x + this._radius - radius;
+      this.y = this.y + this._radius - radius;
+      this.trigger("Change");
+      return this;
+    },
+
+    velocity: function(vx, vy) {
+      this.vx = vx;
+      this.vy = vy;
       this.trigger("Change");
       return this;
     },
